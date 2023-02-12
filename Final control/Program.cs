c3fbcc76d1daeac5c509e36e04f0c4f3fb9fd2ee
Maxim Zaharuk <director.potok@gmail.com>
2023-02-12 21:27:35 +0300

    final solution code

--- Final control/Program.cs
+++ Final control/Program.cs
@@ -1,1 +1,42 @@
-﻿
+﻿int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    int result;
+    int.TryParse(Console.ReadLine(), out result);
+    return result;
+}
+
+string[] CreateArray(int size)
+{
+    string[] arr = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine("Введите элемент массива:");
+        arr[i] += Console.ReadLine();
+    }
+    return arr;
+}
+
+string GetResult(string[] arr)
+{
+    string result = string.Empty;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            result += arr[i] + " ";
+        }
+    }
+    return result;
+}
+
+int arraySize = GetNumber("Введите размер массива:");
+string[] arr = CreateArray(arraySize);
+string result = GetResult(arr);
+string[] finalResult = result.Split("");
+/* если нужна печать, то раскомментировать следующий кусок кода.
+foreach (string item in finalResult)
+{
+    Console.WriteLine(item);
+}
+*/
