3e15cf603a16221853870748d4409144e4ce34ee
Maxim Zaharuk <director.potok@gmail.com>
2023-02-12 21:18:14 +0300

    initial commit and block-scheme

--- Practice9/Program.cs
+++ Practice9/Program.cs
@@ -58,13 +58,16 @@ int Ackerman(int m, int n)
     if (m == 0) 
     {
         return n + 1;
-    } else if (m > 0 && n == 0) 
+    } 
+    else if (m > 0 && n == 0) 
     {
         return Ackerman(m - 1, 1);
-    } else if (m > 0 && n > 0) 
+    } 
+    else if (m > 0 && n > 0) 
     {
         return Ackerman(m - 1, Ackerman(m, n - 1));
-    } else 
+    } 
+    else 
     {
         return 0;
     }
